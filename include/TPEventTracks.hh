69611f61ae43797a77fc4d9432fbb5b2ded2c278
sezata <sezata@gmail.com>
2017-06-13 18:18:40 -0400

    added scint match functionality to TPEventTracks

--- include/TPEventTracks.hh
+++ include/TPEventTracks.hh
@@ -28,52 +28,39 @@ public:
   int GetNTrack() const;
   size_t size() const;
 
+  void SetSciBCID(int sciBCID, int sciph);
+  void SetSciOffset(int offset);
+
+  double SciBCID();
+  int SciOffset();
+
   TPTrack const& Get(int itrack) const;
   TPTrack const& operator [] (int itrack) const;
 
   std::vector<TPTrack*>::iterator begin();
   std::vector<TPTrack*>::iterator end();
 
-  TPTrack* Highlander(const MMClusterList& clusters);
+  TPTrack* Highlander(const MMClusterList& clusters, bool scimatch, int cut=10);
+  double deltaBCID(int BCID);
   
 private:
   std::vector<TPTrack*> m_track;
+
+  double m_sciBCID; 
+  int m_offset;
 
   friend class PDOToCharge;
   friend class TDOToTime;
 };
 
-inline TPTrack* TPEventTracks::Highlander(const MMClusterList& clusters){
-  //
-  // Choose the TPTrack with most hits in common with MMClusterList provided.
-  // If >1 tracks satisfy this, pick the first (theyre sorted by N(hits)).
-  //
-  if (m_track.size() == 0)
-    return nullptr;
-
-  int mostmatch = -1, nmatch = -1;
-  TPTrack* the_one = nullptr;
-
-  for (size_t it = 0; it < m_track.size(); ++it){
-    nmatch = 0;
-    for (size_t ia = 0; ia < m_track[it]->size(); ++ia){
-      if (clusters.ContainsTP(m_track[it]->Get(ia))){
-        nmatch++;
-      }
-    }
-    m_track[it]->SetNMatch(nmatch);
-    if (nmatch > mostmatch){
-      mostmatch = nmatch;
-      the_one = m_track[it];
-    }
-  }
-
-  return the_one;
+inline TPEventTracks::TPEventTracks() {
+  m_sciBCID = -1;
+  m_offset = -1;
 }
 
-inline TPEventTracks::TPEventTracks() {}
-
 inline TPEventTracks::TPEventTracks(const TPTrack& track){
+  m_sciBCID = -1;
+  m_offset = -1;
   AddTrack(track);
 }
 
@@ -121,12 +108,75 @@ inline size_t TPEventTracks::size() const {
   return m_track.size();
 }
 
+inline void TPEventTracks::SetSciBCID(int sciBCID, int sciph) {
+  m_sciBCID = sciBCID+sciph/16.;
+}
+
+inline void TPEventTracks::SetSciOffset(int offset) {
+  m_offset = offset;
+}
+
+inline double TPEventTracks::SciBCID() {
+  return m_sciBCID;
+}
+
+inline int TPEventTracks::SciOffset() {
+  return m_offset;
+}
+
 inline TPTrack const& TPEventTracks::Get(int itrack) const {
   return *m_track[itrack];
 }
 
 inline TPTrack const& TPEventTracks::operator [] (int itrack) const {
   return Get(itrack);
+}
+
+inline TPTrack* TPEventTracks::Highlander(const MMClusterList& clusters, bool scimatch, int cut){
+  //
+  // Choose the TPTrack with most hits in common with MMClusterList provided.
+  // If >1 tracks satisfy this, pick the first (theyre sorted by N(hits)).
+  //
+  if (m_track.size() == 0)
+    return nullptr;
+
+  int mostmatch = -1, nmatch = -1;
+  int nscimatch = -1;
+  if (scimatch && m_sciBCID > 0 && m_offset != -1)
+    nscimatch = 0;
+  TPTrack* the_one = nullptr;
+
+  for (size_t it = 0; it < m_track.size(); ++it){
+    nmatch = 0;
+    if (scimatch && m_sciBCID > 0 && m_offset != -1){
+      if (fabs(deltaBCID(m_track[it]->BCID())) < cut)
+        nscimatch++;
+      else
+        continue;
+    }
+    for (size_t ia = 0; ia < m_track[it]->size(); ++ia){
+      if (clusters.ContainsTP(m_track[it]->Get(ia))){
+        nmatch++;
+      }
+    }
+    m_track[it]->SetNMatch(nmatch);
+    if (nmatch > mostmatch){
+      mostmatch = nmatch;
+      the_one = m_track[it];
+    }
+  }
+
+  if (nscimatch == 0){
+    return nullptr;
+  }
+  return the_one;
+}
+
+inline double TPEventTracks::deltaBCID(int BCID) {
+  if ((m_sciBCID - BCID) < 0)
+    return m_sciBCID - BCID + m_offset;
+  else
+    return m_sciBCID - BCID + (m_offset - 4096);
 }
 
 #endif
